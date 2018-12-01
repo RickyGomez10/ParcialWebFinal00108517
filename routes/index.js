7736f4dacadc3385384b365d92baacf81981668c
RickyGomez10 <00108517@uca.edu.sv>
2018-12-01 17:54:24 -0600

    Se ha terminado el parcial ya calificado en este momento

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,11 @@ var mongoose = require('mongoose');
 router.get('/', function(req, res, next) {
   
   Medico.find({},(err,result)=>{
+
     res.render('index',{title: 'Medicos', FilasBD: JSON.stringify(result)});
-    res.status(200).json({
-      message: "asd"
-    })
-  })
-  
+   
+  });
+
 });
 
 router.post('/',(req,res,next)=>{
@@ -24,14 +23,14 @@ router.post('/',(req,res,next)=>{
 });
 medico.save().then(result =>{
   console.log(result);
-  res.redirect('/');
+  res.redirect('/api/Medicos');
 }).catch(error =>{console.log(error)});
 });
 
 router.get('/eliminar/:valor',(req,res,next) =>{
   Medico.findByIdAndRemove({_id: mongoose.Types.ObjectId(req.params.valor)},(err,result)=>{
     if(result){
-    res.redirect('/');
+    res.redirect('/api/Medicos');
     }
     if(err){
       console.log(err);
@@ -39,16 +38,17 @@ router.get('/eliminar/:valor',(req,res,next) =>{
   });
 
 });
-router.get('/actualizar/:valor',(req,res,next)=>{
-    if(req.body.Nombre){
-      Medico.findByIdAndUpdate({_id:mongoose.Types.ObjectId(req.params.valor)}, {Nombre: req.body.Nombre},function(err,place){
+router.post('/actualizar/:valor',(req,res,next)=>{
+    
+      Medico.findByIdAndUpdate({_id:mongoose.Types.ObjectId(req.params.valor)}, {Nombre: "ValorActualizado"},function(err,place){
         if(place){
           console.log("si se pudo");
+          res.redirect('/api/Medicos');
         }
         if(err){
           console.log(err);
         }
       });
-    }
+    
 });
 module.exports = router;
